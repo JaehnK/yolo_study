9334ad719717ffbcc99fec4d0cc9dd919b3febe4
jaehnK <lecielgris1@gmail.com>
2024-07-25 18:52:01 +0900

    [Add] data_preprocessig get_imgsize

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -12,14 +12,37 @@ def prop_open(path:str) -> dict:
         content = f.readlines()
         for i, con in enumerate(content):
             element = con.split('=')
-            crops = [int(e) for e in element[1].split(',')]
-            cropProp[element[0]] = {'crop_raw' : crops, 'img_size' : None, 'crop_norm' : list()}
+            if element == 2:
+                key, value = element
+                crops = [int(e) for e in value.split(',')]
+                cropProp[key] = {'crop_raw' : crops, 'img_size' : None, 'crop_norm' : list()}
     return cropProp
 
 def get_imgsize(paths:list, img_infos:dict):
-    
-    return
+    for img in paths:
+        im = Image.open(img)
+        filename = os.path.basename(img)
+        key = filename[:-4]  
 
+        if filename not in img_infos.keys():
+            img_infos[key] =  {'crop_raw' : None, 'img_size' : None, 'crop_norm' : list()}
+
+        img_infos[key]['img_size'] = list(im.size)
+        img_size = list(im.size)
+        img_infos[key]['img_size'] = img_size
+            
+        if img_infos[key]['crop_raw'] is None:
+            img_infos[key]['crop_raw'] = [0, 0, img_size[0], img_size[1]]
+        
+        raw_crop = img_infos[key]['crop_raw']
+        width, height = img_size
+        x_center = (raw_crop[0] + raw_crop[2]) / 2 / width
+        y_center = (raw_crop[1] + raw_crop[3]) / 2 / height
+        crop_width = (raw_crop[2] - raw_crop[0]) / width
+        crop_height = (raw_crop[3] - raw_crop[1]) / height
+        
+        img_infos[key]['crop_norm'] = [x_center, y_center, crop_width, crop_height]
+    return
 
 def main():
     path = './foodSample'
@@ -30,8 +53,10 @@ def main():
         img_infos.update(prop_open(dir_path))
         
         imgs_path = [os.path.join(dir_path, img) for img in os.listdir(dir_path) if img.endswith('jpg')]
-        print(imgs_path)
-        #print(img_infos)
+        get_imgsize(imgs_path, img_infos)
+        #print(imgs_path)
+        print(img_infos)
+
 
 if __name__ == '__main__':
     main()
